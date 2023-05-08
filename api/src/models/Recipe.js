eb4f4a72b0e8bbc765464a02d8562bd166e14a6d
EDuqueC7 <esteban.duque911@gmail.com>
2023-05-08 13:11:05 -0500

    Avance02

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,10 +3,40 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('Recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    resumen_plato: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+
+    comida_saludable: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    paso_a_paso: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
   });
 };
