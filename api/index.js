9fd0f8d3c226ace5b18486d1ef4191d709d69354
EDuqueC7 <esteban.duque911@gmail.com>
2023-06-02 11:54:53 -0500

    Deploy

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config()
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001;
 
 
 conn.sync({ force: true }).then(() => {
   console.log("Database connected")
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
+  server.listen(port, () => {
+    console.log(`Server raised in port ${port}`); 
   });
 });
