1ca56969c9b6958c3f4be523b465ea19d7be0af9
EDuqueC7 <esteban.duque911@gmail.com>
2023-06-02 13:00:43 -0500

    Deploy02

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
-
 conn.sync({ force: true }).then(() => {
   console.log("Database connected")
   server.listen(port, () => {
